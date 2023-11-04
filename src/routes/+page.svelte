5ee272b8ed3c28c588ccf146554b751aa14d88ba
rachata.p <rachata.ptnn@gmail.com>
2023-11-04 22:46:19 +0700

    atleast plain ui complete ._.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- src/routes/TestCaseGenerator.svelte -->
+<style>
+    #editor-container {
+      background-color: #272822; /* Background color */
+      color: #F8F8F2; /* Text color */
+      /* Add other styling properties to match the theme */
+    }
+</style>
+<script>
+    let url = '';
+    let responseData = '';
+    let code = '';
+  
+    async function handleSubmit() {
+      if (url) {
+        const payload = JSON.stringify({ "url": url });
+        try {
+          const apiResponse = await fetch("http://localhost:8080/gen-testcases", {
+            method: "POST",
+            body: payload,
+            headers: {
+              "Content-Type": "application/json"
+            }
+          });
+          const data = await apiResponse.json();
+          responseData = JSON.stringify(data, null, 2);
+          code = data.testCases;
+        } catch (error) {
+          responseData = "Error: " + error;
+        }
+      }
+    }
+  </script>
+  
+  <h1>API Request Form</h1>
+  
+  <form on:submit={handleSubmit}>
+    <label for="urlInput">URL:</label>
+    <input type="text" id="urlInput" bind:value={url} placeholder="Enter URL" />
+    <button type="submit">Submit</button>
+  </form>
+  
+  <div id="response">
+    <pre>{responseData}</pre>
+  </div>
+  
+  <div id="editor-container">
+    <pre>{code}</pre>
+  </div>
